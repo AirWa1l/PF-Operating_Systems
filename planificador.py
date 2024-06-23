903bb0f49bf5e9cd5b9800fbf8891a1ba3984613
Jose Adrian Marin <jose.adrian.marin@correounivalle.edu.co>
2024-06-23 15:38:01 -0500

    modificacion del planificador y del archivo contenedor.py

--- planificador.py
+++ planificador.py
@@ -9,20 +9,37 @@ def contenedor_r(image_n, RunTime):
     time.sleep(RunTime)
     process.terminate()
 
-def planf(commands, quantum):
-    while commands:
-        command = commands.pop(0)
-        print(f"Running: {command}")
-        process = subprocess.Popen(command, shell=True)
-        time.sleep(quantum)
-        process.terminate()
-        commands.append(command)
+##Elmines el quantum de aqui(Adr)
+def planf(commands):
+    threads =[]
+    for command, STime, RunTime in commands:
+        thread = threading.Timer(STime, contenedor_r, args = (command,RunTime))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    # while commands:
+    #     command = commands.pop(0)
+    #     print(f"Running: {command}")
+    #     process = subprocess.Popen(command, shell=True)
+    #     time.sleep(quantum)
+    #     process.terminate()
+    #     commands.append(command)
 
 if __name__ == "__main__":
-    commands = [
-        "docker run --rm custom_container_image_1",
-        "docker run --rm custom_container_image_2",
-        "docker run --rm custom_container_image_3",
-    ]
-    quantum = 5
-    planf(commands, quantum)
+    ar_commands = "commands.txt"
+    with open(ar_commands, "r") as f:
+        commands = [line.strip().split(" ") for line in f]
+        commands = [(f"custom_container_image_{i}", int(STime), int(RunTime)) for i, (command, STime, RunTime) in enumerate(commands)]
+    planf(commands)
+
+
+    ##Prueba para el planificador inicial
+    # commands = [
+    #     "docker run --rm custom_container_image_1",
+    #     .............
+    # ]
+    # quantum = 5
+    # planf(commands, quantum)
