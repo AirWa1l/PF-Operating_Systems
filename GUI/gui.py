0f430f60ffbdebdda8a7e8d027824302ba38144f
LjuandalZPH <juanda.190404@hotmail.com>
2024-06-25 21:47:32 -0500

    Correcion inicio

--- GUI/gui.py
+++ GUI/gui.py
@@ -4,9 +4,9 @@ from tkinter import *
 ventana = Tk()
 ventana.title("Inicio")
 ventana.geometry("500x400")
-ventana.grid_rowconfigure(0, weight=3)
-ventana.grid_columnconfigure(0,weight=3)
+
 ventana.resizable(False,False)
+
 #ventana.iconbitmap('https://github.com/AirWa1l/PF-Operating_Systems/blob/GUI/GUI/icono.ico')
 
 
@@ -18,9 +18,9 @@ comandos = Frame(ventana,borderwidth=3)
 inicio.pack(expand=True, fill="both")
 
 #definicion de tamaño de row y columns
-inicio.rowconfigure(0, minsize=20)
+inicio.rowconfigure(0, minsize=50)
 inicio.rowconfigure(1, minsize=50)
-inicio.rowconfigure(2, minsize=50)
+inicio.rowconfigure(2, minsize=10)
 inicio.columnconfigure(0, minsize=50)
 inicio.columnconfigure(1, minsize=50)
 inicio.columnconfigure(2, minsize=50)
@@ -29,20 +29,24 @@ inicio.columnconfigure(2, minsize=50)
 def iniciar():
     inicio.destroy()
     comandos.pack(expand=True, fill="both")
+    ventana.update()
     print("Empezaste la aplicacion")
 
 
 #Etiquetas inicio
-bienvenida = Label(inicio, text="Bienvenido a la app",background= "#E0FFFF",font=("Times New Roman", 16, "italic"), borderwidth=1, relief="sunken")
-integrantes = Label(inicio, text= "Integrantes:")
-p00 = Label(inicio)
+bienvenida = Label(inicio, text="Bienvenido a la app",background= "#E0FFFF",font=("Times New Roman", 16, "italic"), borderwidth=1,relief="sunken")
+integrantes = Label(inicio, text= "Integrantes:", font=("Times New Roman", 14), background="#F5CBA7",relief="ridge")
+mensajeCambiar = Label(inicio, background="#52BE80" ,text="Ingresar comandos", font=("Times New Roman", 14),relief="groove")
 
+integrantes_list = Listbox(inicio)
+nombres = ["Nombre1", "nombre2"]
+for name in nombres:
+    integrantes_list.insert(END,name)
 
-integrantes.grid(row=4,column=4)
-bienvenida.grid(row=0, column=1, columnspan=2, sticky="nsew")
-p00.grid(row=0,column=0)
-
-
+integrantes.grid(row=2,column=1)
+bienvenida.grid(row=0, column=2, columnspan=1, sticky="e")
+mensajeCambiar.grid(row=4,column=3)
+integrantes_list.grid(row=3, column=1, sticky="n")
 
 
 #etiquetas comando
@@ -55,6 +59,6 @@ comando.grid(row=0,column=0)
 
 #botones
 botonStart = Button(inicio, text ="Start", command = iniciar, padx=20)
-botonStart.grid(row=4,column=3)
+botonStart.grid(row=5,column=3, sticky="se", )
 
 ventana.mainloop()
